b05d1469b57f033ea0d8461f1b5120f1a4f9c8ce
thiagonzalezz <thiagogleyes@gmail.com>
2024-10-18 12:25:05 -0300

    first commit

---
+++ src/users/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import Location, Profile
+from .widgets import CustomPictureImageFieldWidget
+
+from localflavor.us.forms import USZipCodeField
+
+class UserForm(forms.ModelForm):
+    username = forms.CharField(disabled=True)
+    
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name')
+
+class ProfileForm(forms.ModelForm):
+    photo = forms.ImageField(widget=CustomPictureImageFieldWidget)
+    bio = forms.TextInput()
+    
+    class Meta:
+        model = Profile
+        fields = ('photo', 'bio', 'phone_number')
+
+
+class LocationForm(forms.ModelForm):
+    
+    address_1 = forms.CharField(required=True)
+    zip_code = USZipCodeField(required=True)
+    
+    class Meta:
+        model = Location
+        fields = {'address_1', 'address_2', 'city', 'state', 'zip_code'}
+
